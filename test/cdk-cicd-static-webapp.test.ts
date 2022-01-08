0c534835bba5336bac47549e72f9b1a1e0b82c30
Duleendra Shashimal <duleendra@bambu.co>
2022-01-08 19:44:28 +0800

    Initial commit

---
+++ test/cdk-cicd-static-webapp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCicdStaticWebapp from '../lib/cdk-cicd-static-webapp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cicd-static-webapp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCicdStaticWebapp.CdkCicdStaticWebappStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
