eff78e2e95f0a52a2d3bbc179d961b73ccd63d0e
Duleendra Shashimal <duleendra@bambu.co>
2022-01-14 01:38:03 +0800

    Updated the config object

--- config/PipelineConfig.ts
+++ config/PipelineConfig.ts
@@ -5,7 +5,7 @@ export const PipelineConfig: IPipelineConfig = {
         repositoryName: 'simple-webapp',
         branchName: 'main',
         owner: 'shashimal',
-        codestarArn: 'arn:aws:codestar-connections:us-east-1:793209430381:connection/4fc878df-93f1-45ef-89c8-3279b53b6ba4'
+        codestarArn: 'arn:aws:codestar-connections:us-east-1:0000:connection/4fc878df-93f1-45ef-89c8-3279b53b6ba4'
     },
     buildStage: {
         buildCommand: 'npm run build',
@@ -13,6 +13,6 @@ export const PipelineConfig: IPipelineConfig = {
         s3BucketRegion: 'us-east-1'
     },
     notification: {
-        slackChannelConfigArn: 'arn:aws:chatbot::793209430381:chat-configuration/slack-channel/cicd-webapp'
+        slackChannelConfigArn: 'arn:aws:chatbot::0000:chat-configuration/slack-channel/cicd-webapp'
     },
 }
